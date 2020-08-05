05473628c507e90ae6bd8341bab8a5104d71ccd2
Martyn Coupland <martyn.coupland@gmail.com>
2020-08-05 18:17:40 +0100

    Added logic to probe before failover
    Probes both nodes, if both fail then failover is not initiated

--- MerakiHA.cs
+++ MerakiHA.cs
@@ -15,54 +15,77 @@ namespace MerakiHAWatch
     public static class MerakiHA
     {
         [FunctionName("MerakiHA")]
-        public static void Run([TimerTrigger("0 */2 * * * *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 */2 * * * *")] TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation($"Function executed at: {DateTime.Now}");
+
             // Meraki dashboard parameters
-            var apiKey = Environment.GetEnvironmentVariable("MerakiApiKey");                
+            var apiKey = Environment.GetEnvironmentVariable("MerakiApiKey");
             var networkId = Environment.GetEnvironmentVariable("MerakiNetworkId");
             var targetIp = Environment.GetEnvironmentVariable("MerakiTargetIp");
             var timespan = Environment.GetEnvironmentVariable("MerakiProbeTimespan");
+
             // UDR parameters
             AadCredential cred = new AadCredential()
             {
-                 TenantId = Environment.GetEnvironmentVariable("AadTenantId"),
-                 ClientId = Environment.GetEnvironmentVariable("AadClientId"),
-                 ClientSecret = Environment.GetEnvironmentVariable("AadClientSecret")
+                TenantId = Environment.GetEnvironmentVariable("AadTenantId"),
+                ClientId = Environment.GetEnvironmentVariable("AadClientId"),
+                ClientSecret = Environment.GetEnvironmentVariable("AadClientSecret")
             };
             var subscription = Environment.GetEnvironmentVariable("AzSubscriptionId");
+
             // route table parameters
             var udrrg = Environment.GetEnvironmentVariable("AzRouteTableResourceGroup");
             var routeTableName = Environment.GetEnvironmentVariable("AzRouteTableName");
             var routeNames = JsonConvert.DeserializeObject<List<object>>(Environment.GetEnvironmentVariable("AzRouteNames"));
+
             // Blob Storage Parameters
             var blobCnString = Environment.GetEnvironmentVariable("BlobCnString");
             var container = Environment.GetEnvironmentVariable("BlobContainerName");
             var blobname = Environment.GetEnvironmentVariable("BlobName");
+
             // Log Analytics parameters            
             var workspaceId = Environment.GetEnvironmentVariable("LogAnalyticsWorkspaceId");
             var workspaceKey = Environment.GetEnvironmentVariable("LogAnalyticsWorkspaceKey");
+
             // max loss percent
             var maxLossPercent = Environment.GetEnvironmentVariable("MaxLossPercent");
+
             // create all dependency injection objects
-            IGetLossLatency probe = new ProbeAgent(apiKey,targetIp);
+            IGetLossLatency probe = new ProbeAgent(apiKey, targetIp);
             IDefineRoute router = new RouteAgent(cred, subscription);
             IPersistFailover blob = new BlobAgent(blobCnString, container, blobname);
             ILog logger = new AzMonLogger(workspaceId, workspaceKey);
+            
             // constructor
             List<RouteInfo> routes = new List<RouteInfo>();
             foreach (var routeName in routeNames)
             {
-                routes.Add(new RouteInfo() {  
-                    ResourceGroup= udrrg,
+                routes.Add(new RouteInfo()
+                {
+                    ResourceGroup = udrrg,
                     RouteTableName = routeTableName,
                     RouteName = routeName.ToString()
                 });
             }
             MerakiHaAgent agent = new MerakiHaAgent(probe, router, blob, logger, routes);
-            // probe and failover if needed
-            var result = agent.ProbeAndFailover(int.Parse(maxLossPercent));
-            log.LogInformation("Loss={0}, Failover={1}, Message={2}", result.lossPercent, result.Failover, result.Message ?? "OK");
+
+            // Probe and failover if needed
+            var activeProbe = agent.CanProbeActive(int.Parse(maxLossPercent));
+            var standbyProbe = agent.CanProbeStandby(int.Parse(maxLossPercent));
+
+            // Don't failover if both nodes are unreachable
+            if (!activeProbe && standbyProbe)
+            {
+                var result = agent.Failover(int.Parse(maxLossPercent));
+                log.LogInformation("Loss={0}, Failover={1}, Message={2}", result.lossPercent, result.Failover, result.Message ?? "OK");
+            }
+
+            // Both nodes are down, don't failover but alert
+            if (!activeProbe && !standbyProbe)
+            {
+                log.LogCritical("Loss={0}, Failover={1}, Message={2}", "100", "false", "Both nodes unreachable, will not failover.");
+            }
         }
     }
 }
